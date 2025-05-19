69906e5e1a8073bae60eebdb53d0d499fb031002
Raxan7 <manyerere201@gmail.com>
2025-05-19 17:51:55 +0300

    Fixing the error

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -21,6 +21,30 @@ subprojects {
             buildFeatures {
                 buildConfig = true
             }
+            
+            // Fix for plugins that don't specify namespace
+            if (project.plugins.hasPlugin("com.android.library")) {
+                val extension = project.extensions.getByType(com.android.build.gradle.LibraryExtension::class.java)
+                if (extension.namespace == null) {
+                    // Try to get package name from manifest
+                    val manifest = project.file("src/main/AndroidManifest.xml")
+                    if (manifest.exists()) {
+                        val manifestContent = manifest.readText()
+                        val packageRegex = "package=\"([^\"]+)\"".toRegex()
+                        val packageMatch = packageRegex.find(manifestContent)
+                        packageMatch?.groupValues?.get(1)?.let { packageName ->
+                            extension.namespace = packageName
+                        } ?: run {
+                            // Default namespace for connectivity_plus if we couldn't find it
+                            if (project.name == "connectivity_plus") {
+                                extension.namespace = "dev.fluttercommunity.plus.connectivity"
+                            }
+                        }
+                    } else if (project.name == "connectivity_plus") {
+                        extension.namespace = "dev.fluttercommunity.plus.connectivity"
+                    }
+                }
+            }
         }
     }
 }
